eb8106cdfee028f0bbef35772ba7bc973fd64634
Sanyam Mahajan <67626072+09-hrick@users.noreply.github.com>
2022-09-10 21:58:57 +0530

    Update project2.cpp

--- project2.cpp
+++ project2.cpp
@@ -28,6 +28,9 @@ class Employee{
     bool loginemp(double empId,string emppin){
         return (_employeeId_==empId && _employeePas_==emppin);
     }
+    ~Employee(){
+
+    }
    
 };
 
@@ -65,7 +68,7 @@ private:
 public:
     Accounts(){
     }
-    Accounts(double  account_number, int    atm_pin,string accountHolderName,double amount,string  account_type){
+    Accounts(double account_number, int atm_pin,string accountHolderName,double amount,string  account_type){
 
         _account_number_ = account_number;
         _atm_pin_ = atm_pin;
@@ -107,6 +110,97 @@ class Bank :private Accounts,private Transationlog,private Employee
 private:
     Accounts cust;
     Employee emp;
+    void logincust(){
+         double anum;int apin;
+            cout<<"Entering Customer login portal\n";
+            cout<<"Are you a existing customer?\npress y for yes or any other key for no: ";
+            char getchary;
+            cin>>getchary;
+
+            if(getchary=='y' || getchary=='Y')
+            {
+                cout<<"Please enter account number: ";
+                cin>>anum;
+                cout<<"Please enter atm pin: ";
+                cin>>apin;
+                for(auto i:accountsvector)
+                {
+                        if(i.loginacc(anum,apin)){
+                        cust=i;
+                        cout<<"login sucessful\n";
+
+                        }
+                        else
+                        {
+                            cout<<"incorrect Atm pin or Account number\n"<<
+                            "Do you still want to continue? \n"<<
+                            "Press 1 to continue or any other key to exit the login portal: ";
+                            if(getchar()=='1')
+                            {
+                            loginportal();
+                            }
+
+                        }
+                }
+                
+            }
+        else{
+            
+            double tempAccNum;
+            int tempPin;
+            string tempAccountHolderName;
+            double tempAmount=0;
+            string tempaccount_type;
+            cout<<"Welcome New customer\n";
+            tempAccNum=accountsvector.back().getaccnum()+1;
+            cout<<"Your account number is: "<< tempAccNum<<endl ;
+            cout<<"Please enter account holder name: ";
+            cin>>tempAccountHolderName;
+            cout<<"Please enter 4 digit atm pin: ";
+            cin>>tempPin;
+            cout<<"1, for savings , 2 for current , 3 for joint\n";
+            cout<<"Please enter account type: ";
+            cin>>tempaccount_type;
+            cout<<"Enter the amount you want to deposit: ";
+            cin>>tempAmount;
+            //Accounts(double account_number, int atm_pin,string accountHolderName,double amount,string  account_type)
+            Accounts tempaccount=Accounts(tempAccNum,tempPin,tempAccountHolderName,tempAmount,tempaccount_type);
+            cout<<"Account created sucessfully\n";
+            accountsvector.push_back(tempaccount);
+            logincust();
+
+        }
+
+    }
+    void loginemp(){
+        cout<<"Entering Employee login Portal\n";
+        double empid;
+        string emppass;
+        cout<<"Please enter employee ID: ";
+        cin>>empid;
+        cout<<"Please enter password: ";
+        cin>>emppass;
+
+        // this->employeesvector[0].display();
+        for(auto i:employeesvector)
+        {
+                if(i.loginemp(empid,emppass)){
+                    emp=i;
+                    cout<<"login sucessful\n";
+                }else{
+                     cout<<"Incorrect password or Account number\n"
+                    <<"Do you still want to continue? \n"<<
+                    "Press y to continue or any other key to exit the login portal: ";
+                    char getchary;
+                    cin>>getchary;
+                    if(getchary=='y' || getchary=='Y'){
+                        loginportal();
+                     }
+                }
+        }
+       
+
+    }
 
 public:
     vector <Accounts> accountsvector;
@@ -114,6 +208,8 @@ public:
     vector <Employee> employeesvector;
     int current_user;
     /* zero customer one employee*/
+
+
     Bank(){
         fstream finout;
         vector<string> sepa;
@@ -156,96 +252,72 @@ public:
             employeesvector.push_back(tempemp);
         } finout.close();
 
+    }
+    
+    
+    /*
+    ~Bank(){
+
+        fstream finout;
+        vector<string> sepa;
+        finout.open("accounts.csv",ios::ate|ios::in|ios::out);
+        finout.seekg(0,ios::beg);
+        //skipping the first line of the read file
+        sepa=getsplitvector(finout);
+        //getting data from the file till the end of file and storing it in a vector of class books
+        while( !finout.eof() ) {
+            sepa=getsplitvector(finout);
+            //double  account_number, int    atm_pin,string accountHolderName,double amount,string  account_type)
+            Accounts tempacc(stod(sepa[0]),stoi(sepa[1]),sepa[2],stod(sepa[3]),sepa[4]);
+            accountsvector.push_back(tempacc);
+        } finout.close();
+
+        vector<string> tsep;
+        finout.open("translog.csv",ios::ate|ios::in|ios::out);
+        finout.seekg(0,ios::beg);
+        //skipping the first line of the read file
+        tsep=getsplitvector(finout);
+        //getting data from the file till the end of file and storing it in a vector of class books
+        while( !finout.eof() ) {
+            tsep=getsplitvector(finout);
+            //double AccountToBelogged, double CreditedOrDebtedAccount, double AmountTransfered, string CreditedOrDebted
+            Transationlog temptrans(stod(tsep[0]),stod(tsep[1]),stod(tsep[2]),tsep[3]);
+            transationlogsvector.push_back(temptrans);
         }
+        finout.close();
+
+        vector<string> esep;
+        finout.open("employee.csv",ios::ate|ios::in|ios::out);
+        finout.seekg(0,ios::beg);
+        //skipping the first line of the read file
+        esep=getsplitvector(finout);
+        //getting data from the file till the end of file and storing it in a vector of class books
+        while( !finout.eof() ) {
+            esep=getsplitvector(finout);
+            //string en,double eid,string ep
+            Employee tempemp(esep[0],stod(esep[1]),esep[2]);
+            employeesvector.push_back(tempemp);
+        } finout.close();
+
+    }*/
+
+
+
 
     void loginportal(){
     int chooseportal;
     cout<<"To enter Customer login portal press '0'"<<"\nTo enter Employee login Portal press '1'\n";
     cin>>chooseportal;
+    current_user = chooseportal;
     while((chooseportal!=0||chooseportal!=1) )
     {
     if (chooseportal== 0)
-    {
-            double anum;int apin;
-            cout<<"Entering Customer login portal\n";
-            cout<<"Are you a existing customer?\npress y for yes or any other key for no: ";
-            if(getchar()=='y' || getchar()=='Y')
-            {
-                cout<<"Please enter account number: ";
-                cin>>anum;
-                cout<<"Please enter atm pin: ";
-                cin>>apin;
-                for(auto i:accountsvector)
-                {
-                        if(i.loginacc(anum,apin)){
-                        cust=i;
-                        cout<<"login sucessful\n";
-
-                        }
-                }
-                cout<<"incorrect Atm pin or Account number\n"<<
-                "Do you still want to continue? \n"<<
-                "Press y to continue or any other key to exit the login portal: ";
-                if(getchar()=='y' || getchar()=='Y'){
-                    loginportal();
-                }
-                else{cout<<"paatanhi kya hogya";}
-            }
-        else{
-            
-            double tempAccNum;
-            int tempPin;
-            string tempAccountHolderName;
-            double tempAmount=0;
-            string tempaccount_type;
-            cout<<"Welcome New customer\n";
-            tempAccNum=accountsvector.back().getaccnum()+1;
-            cout<<"Your account number is: "<< tempAccNum<<endl ;
-            cout<<"Please enter account holder name: ";
-            cin>>tempAccountHolderName;
-            cout<<"Please enter 4 digit atm pin: ";
-            cin>>tempPin;
-            cout<<"1, for savings , 2 for current , 3 for joint\n";
-            cout<<"Please enter account type: ";
-            cin>>tempaccount_type;
-            cout<<"Enter the amount you want to deposit";
-            cin>>tempAmount;
-            Accounts tempaccount(tempAccNum,tempPin,tempAccountHolderName,tempAmount,tempaccount_type);
-            cout<<"Account created sucessfully";
-            accountsvector.push_back(tempaccount);  
-
-        }
+    { 
+        logincust();
         break;
     }
     else if(chooseportal== 1){
-        cout<<"Entering Employee login Portal\n";
-        double empid;
-        string emppass;
-        cout<<"Please enter employee ID: ";
-        cin>>empid;
-        cout<<"Please enter password: ";
-        cin>>emppass;
-
-        // this->employeesvector[0].display();
-        for(auto i:employeesvector)
-        {
-                if(i.loginemp(empid,emppass)){
-                    emp=i;
-                cout<<"login sucessful\n";
-                employeesvector.back();
-
-                }else{
-                     cout<<"Incorrect password or Account number\n"
-                    <<"Do you still want to continue? \n"<<
-                    "Press y to continue or any other key to exit the login portal: ";
-                    if(getchar()=='y' || getchar()=='Y'){
-                        loginportal();
-                     }
-                }
-        }
-       
-
-
+        loginemp();
         break;
     }
     
@@ -255,7 +327,8 @@ public:
        cin>>chooseportal;
     }
     }
-    current_user = chooseportal;
+    
+
     
     }
     Accounts getAccount(){
@@ -266,16 +339,8 @@ public:
     }
     
 
-    void getcurrentuser(){
-    if(current_user==1)
-    {
-        getAccount();
-
-    }
-    else
-    {
-       getemployee();
-    }
+   bool iscurrentuserCustomer(){
+        return 0;
     }
 
 };
@@ -297,6 +362,7 @@ int main()
     <<string(20,'<')<<endl;
     myBank.loginportal();
 
+    // if(myBank.getAccount()){}
 
     cout<<"Do you want to continue Browsing the bank press 0 for yes and 1 for No: ";
     cin>>exit;
